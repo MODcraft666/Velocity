e5fc98e1e8aee1b40ad849b7e269b154549e061a
MODcraft <admin@ncgs.top>
2021-05-09 08:31:14 +0800

    Update VelocityServer.java

--- proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
+++ proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
@@ -488,7 +488,7 @@ public class VelocityServer implements ProxyServer, ForwardingAudience {
    * @param explicitExit whether the user explicitly shut down the proxy
    */
   public void shutdown(boolean explicitExit) {
-    shutdown(explicitExit, Component.text("Proxy shutting down."));
+    shutdown(explicitExit, Component.text("服务器已关闭/重启中."));
   }
 
   @Override
